6abcb54605d0222060b09cb1f3ea389ca4e2f18a
Vincent Foley <vfoley@gmail.com>
2015-05-12 10:58:58 -0400

    Modified babai test to be executable from script

--- runtests.py
+++ runtests.py
@@ -6,6 +6,7 @@ import pprint
 import os
 import sys
 import tempfile
+import time
 
 EPSILON = 0.001
 MATLAB_HEADER_LINES = 10
@@ -38,9 +39,20 @@ def run_javascript(directory, func, arg):
 
 
 def main():
+    t1 = time.time()
     matlab_results = run_matlab(sys.argv[1], sys.argv[2], sys.argv[3])
+    t2 = time.time()
+    matlab_time = t2 - t1
+
+
+    t1 = time.time()
     javascript_results = run_javascript(sys.argv[1], sys.argv[2], sys.argv[3])
+    t2 = time.time()
+    javascript_time = t2 - t1
+
     if matlab_results == javascript_results:
+        print "MATLAB    : %.5f" % matlab_time
+        print "JavaScript: %.5f" % javascript_time
         sys.exit(0)
     else:
         print(matlab_results)
